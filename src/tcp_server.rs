fa66bc7913889ab28378fc00b166ac9af3afb828
Yiran Shi <yiran0811@outlook.com>
2025-02-13 21:51:09 -0500

    updated handle conn loop

--- src/tcp_server.rs
+++ src/tcp_server.rs
@@ -13,9 +13,9 @@ pub fn tcp_server(addr: SocketAddrV4) -> Result<(), anyhow::Error> {
     let listener = TcpListener::bind(addr).unwrap();
     eprintln!("Server bound successfully, waiting for connections...");
     
-    loop {
-        match listener.accept() {
-            Ok((stream, _)) => {
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
                 eprintln!("New connection accepted from {:?}", stream.peer_addr());
                 thread::spawn(move || {
                     if let Err(e) = handle_conn(stream) {
@@ -26,10 +26,11 @@ pub fn tcp_server(addr: SocketAddrV4) -> Result<(), anyhow::Error> {
             }
             Err(e) => {
                 eprintln!("Error accepting connection: {:?}", e);
-                continue;
             }
         }
     }
+
+    Ok(())
 }
 
 fn handle_conn(stream: TcpStream) -> Result<(), anyhow::Error> {
@@ -39,30 +40,20 @@ fn handle_conn(stream: TcpStream) -> Result<(), anyhow::Error> {
     let mut client_conn = ClientWorkPacketConn::new(&stream);
     let mut server_conn = ServerWorkPacketConn::new(&stream);
     
-    eprintln!("[{}] Waiting to receive work packet...", peer_addr);
-    let work_packet = match client_conn.recv_work_msg() {
-        Ok(packet) => {
-            eprintln!("[{}] Successfully received work packet", peer_addr);
-            packet
-        }
-        Err(e) => {
-            eprintln!("[{}] Failed to receive work packet: {:?}", peer_addr, e);
-            return Err(e);
-        }
-    };
-    
-    eprintln!("[{}] Processing work packet...", peer_addr);
-    let server_work_packet = work_packet.do_work();
-    
-    eprintln!("[{}] Sending response packet...", peer_addr);
-    match server_conn.send_work_msg(server_work_packet) {
-        Ok(_) => eprintln!("[{}] Successfully sent response", peer_addr),
-        Err(e) => {
-            eprintln!("[{}] Failed to send response: {:?}", peer_addr, e);
+    loop {
+        let work_packet = match client_conn.recv_work_msg() {
+            Ok(packet) => {
+                packet
+            }
+            Err(e) => {
+                return Err(e);
+            }
+        };
+        
+        let server_work_packet = work_packet.do_work();
+        
+        if let Err(e) = server_conn.send_work_msg(server_work_packet) {
             return Err(e);
         }
     }
-    
-    eprintln!("[{}] Connection handled successfully", peer_addr);
-    Ok(())
 }
